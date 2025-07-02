d76fcca721cbbd035b6a27901dce84f27804493c
AnishaDhoot <anishadhoot26@gmail.com>
2025-07-03 04:32:22 +0530

    feat: made navbar sticky and a few other changes

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,19 +1,14 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import Dcverse_logo from "@/app/assets/Dcverse_logo.png";
+
 
 export default function Hero() {
   return (
     <section className="relative w-full h-screen min-h-[400px] bg-black overflow-hidden">
-      <header className="fixed top-0 left-0 right-0 h-[8vh] min-h-[40px] bg-black z-90 flex items-center justify-center px-2 sm:px-0">
-        <Image 
-          src={Dcverse_logo} 
-          alt="DCverse Logo" 
-          className="h-[80%] max-h-[36px] sm:max-h-[60px] object-contain" 
-          style={{ width: 'auto' }} 
-        />
-      </header>
+      
+      
+      
       <video
         src={"/herovideo.mp4"}
         autoPlay
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,27 @@ import TrustedBy from "./components/TrustedBy";
 import Features from "./components/faetures";
 import Form from "./components/form";
 import Footer from "./components/footer";
-
+import Dcverse_logo from "@/app/assets/Dcverse_logo.png";
 import PoweredBy from "./components/PoweredBy";
 import VirtualHumans from "./components/new";
+import Image from "next/image";
 
 import FashionInfluencers from "./components/new2";
 
 export default function Home() {
   return (
     <div
-      className="w-full bg-black bg-[url('/background_1.png')] bg-repeat bg-[length:300px_300px]"
+      className="w-full relative bg-black bg-[url('/background_1.png')] bg-repeat bg-[length:300px_300px]"
     >
+      
+      <div className="fixed top-0 left-0 right-0 h-[8vh] min-h-[40px] bg-black z-10 flex items-center justify-center px-2 sm:px-0 border-b-1 border-white">
+        <Image 
+          src={Dcverse_logo} 
+          alt="DCverse Logo" 
+          className="h-[80%] max-h-[36px] sm:max-h-[60px] object-contain" 
+          style={{ width: 'auto' }} 
+        />
+      </div>
       <div className="backdrop-blur-sm backdrop-brightness-[0.6]">
         <DCVerse />
         
